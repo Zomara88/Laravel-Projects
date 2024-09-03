78c2a1d431ccf7e1c18f17a2e017d170f34f8c56
Bryan Choo <152231278+Zomara88@users.noreply.github.com>
2024-09-03 01:03:25 -0500

    Update Post.php

--- ourfirstapp/app/Models/Post.php
+++ ourfirstapp/app/Models/Post.php
@@ -12,6 +12,6 @@ class Post extends Model
     protected $fillable = ['title', 'body', 'user_id'];
 
     public function user() {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id'); // A blog post (Post.php) belongs to a user (User.php). Laravel can do the SQL statement lookup for 'user_id'
     }
 }
